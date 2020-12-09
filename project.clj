8163f95605dd32cdb38279d827f9863f15fe167d
Shogo Ohta <athos0220@gmail.com>
2020-12-10 00:01:11 +0900

    Move Clojure dep under provided profile

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/athos/power-dot"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :profiles {:provided
+             {:dependencies [[org.clojure/clojure "1.10.1"]]}}
   :repl-options {:init-ns user})
