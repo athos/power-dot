12da67e30ebf678871a3d8b2ed6493ef4542d0b5
Shogo Ohta <athos0220@gmail.com>
2021-01-24 00:06:18 +0900

    Add more test cases for better coverage

--- test/power_dot/core_test.clj
+++ test/power_dot/core_test.clj
@@ -3,6 +3,7 @@
             [clojure.test :refer [deftest is]]
             [power-dot.core :as dot])
   (:import [java.util ArrayList Optional]
+           [java.util.concurrent ForkJoinPool]
            [java.util.stream Collectors IntStream]))
 
 (deftest functional-interface?-test
@@ -22,12 +23,20 @@
 (deftest infer-type-test
   (is (= (class +) (infer +)))
   (is (= String (infer s)))
+  (is (= CharSequence (infer ^CharSequence s)))
   (is (= (Class/forName "[I") (infer arr)))
   (is (= (Class/forName "[J") (infer arr')))
   (let [d 12.3]
     (is (= Double/TYPE (infer d))))
   (is (= Class (infer Optional)))
   (is (= Class (infer java.util.UUID))))
+
+(deftest get-matching-method
+  (is (= (.getMethod ForkJoinPool "submit" (into-array [Runnable]))
+         (#'dot/get-matching-method false ForkJoinPool "submit" [Runnable])))
+  (is (thrown? Throwable
+               (#'dot/get-matching-method false ForkJoinPool "submit"
+                                          [(class compare)]))))
 
 (deftest dot-test
   (is (= "FOO"
@@ -45,7 +54,10 @@
   (is (= 15
          (-> (ArrayList. [1 2 3 4 5])
              (.stream)
-             (.collect (dot/. Collectors reducing 0 +))))))
+             (.collect (dot/. Collectors reducing 0 +)))))
+  (let [xs (ArrayList. [3 1 4 1 5])]
+    (dot/. java.util.Collections (sort xs compare))
+    (is (= [1 1 3 4 5] xs))))
 
 (deftest dotdot-test
   (is (= (apply + [1 3 5 7 9])
