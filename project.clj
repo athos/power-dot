65619df343c6a0f1cb9e3dcaf2be2269a6b2a086
Shogo Ohta <athos0220@gmail.com>
2021-02-01 20:15:21 +0900

    Update type-infer

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/athos/power-dot"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[type-infer "0.1.0"]]
+  :dependencies [[type-infer "0.1.1"]]
   :profiles {:provided
              {:dependencies [[org.clojure/clojure "1.10.2"]]}}
   :repl-options {:init-ns user})
